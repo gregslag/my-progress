5fc5595a213c841817b942f44a01fe90a7b69616
Gregory Lagranha <gregslag@10-41-48-82.0200.epoa.br.iosda.org>
2020-01-14 15:49:27 -0300

    first commit

---
+++ my-progress/ContentView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  my-progress
+//
+//  Created by Gregory Lagranha on 14/01/20.
+//  Copyright © 2020 Gregory Lagranha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
