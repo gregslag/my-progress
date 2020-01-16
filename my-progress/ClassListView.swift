d7e9aed9270278ef3745e24fd71868130cfdfbfc
Thomas Moraes <thomasmoraes@10-41-48-95.0200.epoa.br.iosda.org>
2020-01-16 17:08:55 -0300

    Navigation link and modal

--- my-progress/ClassListView.swift
+++ my-progress/ClassListView.swift
@@ -10,26 +10,40 @@ import SwiftUI
 
 struct ClassesListView: View {
     var classes: [ClassObject] = [ClassObject(name: "Bepid", description: "aloaloalo", dates:["01-01-2020","02-01-2020","03-01-2020","04-01-2020","05-01-2020","06-01-2020","07-01-2020"]), ClassObject(name: "Fast Foundation",description:"bbbbbb",dates: ["01-01-2020","02-01-2020","03-01-2020","04-01-2020","05-01-2020","06-01-2020","07-01-2020"]), ClassObject(name: "Foundation", description: "AAAAA",dates:["01-01-2020","02-01-2020","03-01-2020","04-01-2020","05-01-2020","06-01-2020","07-01-2020"]  )]
+    
+    
+    @State var showingDetail = false
+    
+    
     var body: some View {
-        List(){
-            ForEach (classes) { c in
-                NavigationLink(destination: DescriptionClassView(classObject: c )){
-                    HStack{
-                        Text(c.name)
-                        Spacer()
-                       Button(action: {
-                            print("plain")
-                        }) {
-                            Image(systemName: "person.and.person")
-                                .foregroundColor(.blue)
-                                .frame(width: 75, height: 30, alignment: .trailing)
-
-                            
+        ZStack {
+            List(){
+                ForEach (classes) { c in
+                    
+                    NavigationLink(destination: DescriptionClassView(classObject: c )){
+                        HStack{
+                            Text(c.name)
+                            Spacer()
+                            //                        NavigationLink(destination: StudentListView(classObject: c)){
+                            Button(action: {
+                                self.showingDetail.toggle()
+                            }) {
+                                Image(systemName: "person.and.person")
+                                    .foregroundColor(.blue)
+                            }.sheet(isPresented: self.$showingDetail) {
+                                StudentListView(classObject: c)
                             }
-                            .buttonStyle(PlainButtonStyle())
+                                //                            }
+                                
+                                .buttonStyle(PlainButtonStyle())
+                        }
                     }
                 }
             }
-        }.navigationBarTitle("Classes")
+            .navigationBarTitle("Classes")
+            AddButton()
+        }
     }
+    
 }
+
